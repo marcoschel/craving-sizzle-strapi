fab778740d46562919b76d353a9fcdbd8aa998c7
marcoschel <media.schel@gmail.com>
2025-10-01 00:48:26 +0200

    chore: upgrade Strapi to v4.25.23

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'supersecret'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
