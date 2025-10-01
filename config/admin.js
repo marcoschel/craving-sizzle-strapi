c2de381b0fbef0ed09d2473e103b5055df817b15
marcoschel <media.schel@gmail.com>
2025-10-01 12:11:32 +0200

    Update admin.js 1

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'supersecret'),
+    secret: env('ADMIN_JWT_SECRET', 'j15D4YqP4diTN1OtTC9v4/tp4roNU09iMlMdjU3Iazc='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
   },
 });
