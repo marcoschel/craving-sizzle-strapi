28fb42a25f433214fec9fd61d4643b6e8cc214da
marcoschel <media.schel@gmail.com>
2025-10-01 12:26:06 +0200

    Update config/env/production server.js 1

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
+  proxy: true,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS', ['122761ea733b24935f5e1378a4077541689d69138035ef8ca29202966e7df999', 'f4ef60d41ac722144f693cf770a23459264f42d07c9b5b8478183b6de5e45e0f']),
+  },
   url: env("RENDER_EXTERNAL_URL"),
 });
